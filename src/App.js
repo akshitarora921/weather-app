5f481d1bc83389d87e45c7de45bfd304ac43f242
Akshit Arora <akshitarora9211@gmail.com>
2020-07-07 17:53:08 +0530

    logic done

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
-import react from '
+import React, {useState} from 'react'
+import { fetchWeather } from './api/fetchWeather'
+const App = () => {
+    const [city, setCity]= useState('')
+    const [weather, setWeather]= useState({})
+    const search=async(e)=>{
+        // url='http://api.openweathermap.org/data/2.5/weather?q=kaithal&appid=915f1aa716dfff8ef893761367841f57'
+        if(e.key==='Enter')
+        {
+            console.log(city)
+            const data = await fetchWeather(city)
+            setWeather(data)
+            setCity('')
+        } 
+         
+    }
+    return (
+        <div>
+            <h2>My weather App</h2>
+            <input type='text' value={city} onChange={(e)=>{setCity(e.target.value)}} on onKeyPress={search}/>
+            <h4>{weather.name}</h4>
+        </div>
+    )
+}
+export default App
+
---
+++ src/api/fetchWeather.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+const url = 'http://api.openweathermap.org/data/2.5/weather'
+const app_id='915f1aa716dfff8ef893761367841f57'
+export const fetchWeather= async(city)=>{
+    const {data} = await axios.get(url,{
+        params:{
+            q:city,
+            units:'metric',
+            APPID:app_id
+        }
+    })
+    console.log(data)
+    return data
+}
